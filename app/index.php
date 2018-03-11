3ba0c99ebc192c4ea0b993d20fd197748cc1755d
James Cook <james@monocuit.co.uk>
2018-03-11 10:23:12 -0700

    Update with latest changes

--- app/index.php
+++ app/index.php
@@ -1,25 +1,19 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>stitchedinsaltaire.co.uk</title>
+        <link rel="stylesheet" type="text/css" href="style.css"></link>
+
+        <title>Stitched in Saltaire</title>
     </head>
     <body>
         <?php
-            $db_handle = mysql_connect("localhost", "jcook", "jamesjamesjames");
-            $db_found = mysql_select_db("stitchedinsaltaire_co_uk", $db_handle);
-
-            if($db_found)
-            {
-                $result = mysql_query("SELECT * FROM example WHERE id=1");
-                $db_field = mysql_fetch_assoc($result);
-                print $db_field['data'];
-                mysql_close($db_handle);
-            }
-            else
-            {
-                print "Construction is under way...";
-            }
+        printf('<img src="sis.jpg">');
+        printf('<p>This site is currently under construction. Please email any queries to <a href="mailto:stefficook@hotmail.com">stefficook@hotmail.com</a>.</p>');
         ?>
     </body>
 </html>
